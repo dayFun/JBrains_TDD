7ac8c5d8ca606ecbad9fe4e202d7bc3e85a127be
dayFun <joethorngren@gmail.com>
2015-11-05 18:59:22 -0600

    Implemented GCD.

---
+++ test/src/com/dayFun/math/test/GreatestCommonDivisorTest.java
@@ -0,0 +1,50 @@
+package com.dayFun.math.test;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class GreatestCommonDivisorTest {
+
+    @Test
+    public void testExamples() throws Exception {
+        assertEquals(1, gcd(1, 1));
+        assertEquals(2, gcd(2, 2));
+        assertEquals(1, gcd(-1, -1));
+    }
+
+    @Test
+    public void testRelativelyPRime() throws Exception {
+        assertEquals(1, gcd(2, 3));
+        assertEquals(1, gcd(4, 7));
+        assertEquals(1, gcd(-3, -2));
+    }
+
+    @Test
+    public void testOneMultipleOfTheOther() throws Exception {
+        assertEquals(3, gcd(3, 9));
+        assertEquals(5, gcd(5, 30));
+    }
+
+    @Test
+    public void testCommonFactor() throws Exception {
+        assertEquals(2, gcd(6, 8));
+        assertEquals(7, gcd(49, 315));
+        assertEquals(4, gcd(-24, -32));
+    }
+
+    @Test
+    public void testNegatives() throws Exception {
+        assertEquals(4, gcd(-24, 28));
+        assertEquals(-4, gcd(24, -28));
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int t = b;
+            b = a % t;
+            a = t;
+        }
+        return Math.abs(a);
+    }
+}
